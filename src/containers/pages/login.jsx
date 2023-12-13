6ebc19a49f125d488af2dd6d905802181115ddb3
ItzelSelena <selena65524@live.com>
2023-12-13 15:05:56 -0600

    divs centrado solo en edge por el momento si

--- src/containers/pages/login.jsx
+++ src/containers/pages/login.jsx
@@ -24,20 +24,20 @@ function Login() {
           <div className="container">
             <div className="login-register">
               <div className="nav-buttons">
-                <button onClick={switchToLogin} className={activeForm === 'login' ? 'active' : ''}>Login</button>
-                <button onClick={switchToRegister} className={activeForm === 'register' ? 'active' : ''}>Register</button>
+                <button onClick={switchToLogin} className={activeForm === 'login' ? 'active' : ''}>Inicio de sesión</button>
+                {/*<button onClick={switchToRegister} className={activeForm === 'register' ? 'active' : ''}>Register</button> */}
               </div>
               <div className="form-group">
                 {activeForm === 'login' && (
                   <form action="" id="loginform">
-                    <label htmlFor="username">username</label>
+                    <label htmlFor="username">usuario</label>
                     <input type="text" id="username" />
-                    <label htmlFor="password">password</label>
+                    <label htmlFor="password">contraseña</label>
                     <input type="text" id="password" />
                     <input type="submit" value="Submit" className="submit" />
                   </form>
                 )}
-                {activeForm === 'register' && (
+                {/*{activeForm === 'register' && (
                   <form action="" id="registerform">
                     <label htmlFor="fullname">fullname</label>
                     <input type="text" id="fullname" />
@@ -48,11 +48,12 @@ function Login() {
                     <label htmlFor="confirmpassword">confirm password</label>
                     <input type="text" id="confirmpassword" />
                     <input type="submit" value="Submit" className="submit" />
-                  </form>
-                )}
+                  </form>   
+                )} */} 
+
               </div>
               <div id="forgot">
-                <a href="">forgot password?</a>
+                <a href="">¿Olvidaste tu contraseña?</a>
               </div>
             </div>
           </div>
