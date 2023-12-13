f56e9f731b1fc0d4b92c31979be1123311512dc7
matamancos3000 <135998550+matamancos3000@users.noreply.github.com>
2023-12-12 22:25:35 -0600

    sin pc

--- src/containers/pages/home.jsx
+++ src/containers/pages/home.jsx
@@ -9,7 +9,7 @@ function Home() {
             <Layout>
                 <main className="flex-grow overflow-auto">
                     <div className="relative max-w-7xl mx-auto z-10">
-                        {/* Contenido principal de tu página de inicio */}
+                        {/* Contenido principal de tu página de inicio in PC*/}
                       
                     </div>
                 </main>
