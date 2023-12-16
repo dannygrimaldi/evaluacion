9a4dacefd8cb41dd9906c578e74c90ee22c8f81e
matamancos3000 <135998550+matamancos3000@users.noreply.github.com>
2023-12-16 12:27:59 -0600

    se agregan los divs y un ejemplo de imgaen

--- src/containers/pages/home.jsx
+++ src/containers/pages/home.jsx
@@ -1,6 +1,8 @@
 import Header from '../../components/header';
 import Footer from '../../components/footer';
 import Layout from '../../hocs/layouts/Layout';
+import React from "react";
+import {User} from "@nextui-org/react";
 
 function Home() {
     return (
@@ -11,6 +13,54 @@ function Home() {
                     <div className="relative max-w-7xl mx-auto z-10">
                         {/* Contenido principal de tu página de inicio ahora en laptop se agrega otra vez en laptop*/}
                       
+
+
+<div class="flex-container">
+  
+    <div class="item1">
+        <div class="flex-profile">
+  
+        <div class="box1">aqui va algun texto</div>
+        <div class="box2">
+        <User   
+      name="Jane Doe"
+      description="Product Designer"
+      avatarProps={{
+        src: "https://i.pravatar.cc/150?u=a04258114e29026702d"
+      }}
+    />
+        </div>
+        <div class="box3">algo para atras</div>
+
+        </div>
+   
+   
+   
+    </div>
+    <div class="item2">
+        <div class="flex-list">
+            <div class="flex-buscar">
+            <div class="buscar1">AQUI ESTA LA BARRA DE BUSQUEDA </div>
+            <div class="buscar2">BOTON CUALQUIERA</div>
+
+
+
+        </div>
+
+
+
+            <div class="list2">
+                <div>LISTA DE ASESORES</div>
+                
+                </div>
+
+  </div>
+
+   
+   
+        </div>
+
+</div>
                     </div>
                 </main>
             </Layout>
