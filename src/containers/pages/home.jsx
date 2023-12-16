9f0b078ec3bc78c5ffaa2c519dd73fcfb20f00c6
matamancos3000 <135998550+matamancos3000@users.noreply.github.com>
2023-12-16 13:55:20 -0600

    buscar y error

--- src/containers/pages/home.jsx
+++ src/containers/pages/home.jsx
@@ -2,7 +2,7 @@ import Header from '../../components/header';
 import Footer from '../../components/footer';
 import Layout from '../../hocs/layouts/Layout';
 import React from "react";
-import {User} from "@nextui-org/react";
+import {Card, CardFooter, Image, Button} from "@nextui-org/react";
 
 function Home() {
     return (
@@ -15,33 +15,55 @@ function Home() {
                       
 
 
-<div class="flex-container">
+<div className="flex-container">
   
-    <div class="item1">
-        <div class="flex-profile">
+    <div className="item1">
+        <div className="flex-profile">
   
-        <div class="box1">aqui va algun texto</div>
-        <div class="box2">
-        <User   
-      name="Jane Doe"
-      description="Product Designer"
-      avatarProps={{
-        src: "https://i.pravatar.cc/150?u=a04258114e29026702d"
-      }}
-    />
+        <div className="box1">aqui va algun texto</div>
+        <div className="box2">
+        <Card
+      isFooterBlurred
+      radius="lg"
+      className="border-none"
+    >
+      <Image
+        alt="Woman listing to music"
+        className="object-cover"
+        height={200}
+        src="src\assets\img\img-profile.jpg"
+        width={200}
+      />
+      <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+        <p className="text-tiny text-white/80">Available soon.</p>
+        <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg" size="sm">
+          Notify me
+        </Button>
+      </CardFooter>
+    </Card>
+  
+
         </div>
-        <div class="box3">algo para atras</div>
+        <div className="box3">algo para atras</div>
 
         </div>
    
    
    
     </div>
-    <div class="item2">
-        <div class="flex-list">
-            <div class="flex-buscar">
-            <div class="buscar1">AQUI ESTA LA BARRA DE BUSQUEDA </div>
-            <div class="buscar2">BOTON CUALQUIERA</div>
+    <div className="item2">
+        <div className="flex-list">
+            <div className="flex-buscar">
+            <div className="buscar1"><div class="main">
+  
+  
+  <div class="form-group has-feedback has-search">
+    <span class="glyphicon glyphicon-search form-control-feedback"></span>
+    <input type="text" class="form-control" placeholder="Search">
+  </div>
+  
+</div> </div>
+            <div className="buscar2">BOTON CUALQUIERA</div>
 
 
 
@@ -49,7 +71,7 @@ function Home() {
 
 
 
-            <div class="list2">
+            <div className="list2">
                 <div>LISTA DE ASESORES</div>
                 
                 </div>
