d75462f73cec24a4017cb7b5527441efe1b910a8
dannygrimaldi <71056766+dannygrimaldi@users.noreply.github.com>
2024-01-03 11:35:37 -0600

    Se modifica el proyecto

---
+++ src/pages/Course.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Course = () => {
+    return <div className='dark:text-white'>Course</div>
+};
+
+export default Course;
---
+++ src/pages/Profile.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Profile = () => {
+    return <div className='dark:text-white'>Profile</div>
+}
+
+export default Profile
